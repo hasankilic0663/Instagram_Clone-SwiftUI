bb3f21c117d4b018d854b4435e49bde6a7884b2d
hasankilic <hasankilic0663@hotmail.com>
2024-07-16 01:37:51 +0300

    Profile updated.

---
+++ instaCloneBootcamp/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  instaCloneBootcamp
+//
+//  Created by Hasan on 16.07.2024.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
---
+++ instaCloneBootcamp/Core/Profile/ProfileView.swift
@@ -0,0 +1,83 @@
+//
+//  ProfileView.swift
+//  instaCloneBootcamp
+//
+//  Created by Hasan on 16.07.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack{
+            //HEADER
+            VStack(spacing: 10){// butun yazı vb. hepsinin arasını bir tık actık
+                //Profil Kartı
+                HStack{
+                    Image("hasan")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80,height: 80)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)//yuvarlak profil yapıyo resmi
+                    Spacer()
+                        
+                    HStack(spacing:8){
+                        UserStatView(value: 3, title: "Gönderi")//bunu component halıne getırerek kodu asırı derecede aazaltmıs olduk
+                        UserStatView(value: 467, title: "Takipçi")
+                        UserStatView(value: 670, title: "Takip")
+                    }
+                   
+                    
+                    
+                }
+                .padding(.horizontal)
+                
+                //İsim ve Açıklama Kısmı
+                VStack(alignment:.leading , spacing: 4){
+                    Text("Hasan Hüseyin Kılıç")
+                        .font(.footnote)//küçülttü
+                        .fontWeight(.semibold)//yazıyı kalın yaptı
+                    
+                    Text("Merhaba, Bu bir developerım!")
+                        .font(.footnote)
+                    
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,alignment: .leading)// bunuda yazmamız gerekıyor sebebı ıse yatay genıslık ekran kadar olsun ve soldan baslatsın
+                .padding(.horizontal)//sadece yanlardan padding ver
+                
+                //Edit Profile BUtonu
+                Button {
+                    
+                } label: {
+                    Text("Edit Profil")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .frame(width: 360,height: 32)
+                        .foregroundStyle(Color("buttonTextColor"))
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(.gray,lineWidth:1)//stroke ise içinin rengini siliyo dis cerceve yapıyo sadece
+                            
+                        
+                        )
+
+                }
+                Divider()
+
+            }
+            //POSTS
+         
+            LazyVGrid(columns:){
+                
+            }
+            }
+        
+            
+            
+    }
+}
+
+#Preview {
+    ProfileView()
+}
