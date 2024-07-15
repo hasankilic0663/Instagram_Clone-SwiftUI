69428fcf31e23ac186fb7d9148da271ae05dffe1
hasankilic <hasankilic0663@hotmail.com>
2024-07-16 00:22:05 +0300

    Dosyalar oluşturuldu.

---
+++ instaCloneBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  instaCloneBootcamp
+//
+//  Created by Hasan on 16.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ instaCloneBootcamp/instaCloneBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaCloneBootcampApp.swift
+//  instaCloneBootcamp
+//
+//  Created by Hasan on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct instaCloneBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
